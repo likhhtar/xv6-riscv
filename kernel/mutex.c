b9421a1e32ab757222f0648561f9853dff61ab96
Anna <annalikhtar5@mail.ru>
2024-04-19 23:40:31 +0300

    sad attempt

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -9,38 +9,34 @@
 #include "stat.h"
 #include "proc.h"
 
-typedef struct mutex_table{
-    struct spinlock spin;
-    struct sleeplock sleep;
-    mutex_t mutexes[NMUTEX];
-} mtable;
-
-mtable mt;
+// глобальная таблица мьютексов
+struct mutex mutexes[NMUTEX];
 
 void
 mutex_init(void) {
-    for (int i = 0 ; i < NMUTEX; i++) {           // Проход по всем элементам массива mutexes
-        initlock(&mt.mutexes[i].spin, "spinlock"); // Инициализация спин-блокировки для каждого мьютекса
-        initsleeplock(&mt.mutexes[i].sleep, "sleeplock"); // Инициализация блокировки сон для каждого мьютекса
-        mt.mutexes[i].count = 0;                   // Установка начального значения счетчика использования мьютекса
+    struct mutex *mt;
+    for (mt = mutexes; mt < &mutexes[NMUTEX]; mt++) {           // Проход по всем элементам массива mutexes
+        initlock(&mt->spin, "spinlock"); // Инициализация спин-блокировки для каждого мьютекса
+        initsleeplock(&mt->sleep, "sleeplock"); // Инициализация блокировки сон для каждого мьютекса
+        mt->count = 0;                   // Установка начального значения счетчика использования мьютекса
     }
 }
 
 int
 mutex_lock(int mutex_desc) {
     if (mutex_desc < 0 || mutex_desc >= NMUTEX) return -1; // Проверка на корректность описателя мьютекса
-    if (mt.mutexes[mutex_desc].count < 0 || mt.mutexes[mutex_desc].count >= NMUTEX)
+    if (mutexes[mutex_desc].count < 0 || mutexes[mutex_desc].count >= NMUTEX)
         return -2; // Проверяем, что процесс владеет мьютексом с данным дескриптором
-    acquiresleep(&mt.mutexes[mutex_desc].sleep);           // Захват блокировки сон для безопасного доступа к мьютексу
+    acquiresleep(&mutexes[mutex_desc].sleep);           // Захват блокировки сон для безопасного доступа к мьютексу
     return 0;                                              // Возвращаем 0 в случае успешного захвата мьютекса
 }
 
 int
 mutex_unlock(int mutex_desc) {
     if (mutex_desc < 0 || mutex_desc >= NMUTEX) return -1; // Проверка на корректность описателя мьютекса
-    if (mt.mutexes[mutex_desc].count < 0 || mt.mutexes[mutex_desc].count >= NMUTEX)
+    if (mutexes[mutex_desc].count < 0 || mutexes[mutex_desc].count >= NMUTEX)
         return -2; // Проверяем, что процесс владеет мьютексом с данным дескриптором
-    releasesleep(&mt.mutexes[mutex_desc].sleep);           // Освобождение блокировки сон после завершения операций с мьютексом
+    releasesleep(&mutexes[mutex_desc].sleep);           // Освобождение блокировки сон после завершения операций с мьютексом
     return 0;                                              // Возвращаем 0 в случае успешного освобождения мьютекса
 }
 
@@ -48,13 +44,13 @@ mutex_unlock(int mutex_desc) {
 int
 mutex_create(void) {
     for (int i = 0; i < NMUTEX; i++) {  // Проход по массиву mutexes
-        acquire(&mt.mutexes[i].spin);   // Захватываем блокировку для безопасного доступа к текущему мьютексу
-        if (mt.mutexes[i].count == 0) {   // Проверяем, свободен ли текущий мьютекс
-            mt.mutexes[i].count++;        // Увеличиваем счетчик использования текущего мьютекса
-            release(&mt.mutexes[i].spin); // Освобождаем блокировку после завершения операций с текущим мьютексом
+        acquire(&mutexes[i].spin);   // Захватываем блокировку для безопасного доступа к текущему мьютексу
+        if (mutexes[i].count == 0) {   // Проверяем, свободен ли текущий мьютекс
+            mutexes[i].count++;        // Увеличиваем счетчик использования текущего мьютекса
+            release(&mutexes[i].spin); // Освобождаем блокировку после завершения операций с текущим мьютексом
             return i;                     // Возвращаем индекс свободного мьютекса
         }
-        release(&mt.mutexes[i].spin);     // Освобождаем блокировку, если текущий мьютекс занят, перед переходом к следующему
+        release(&mutexes[i].spin);     // Освобождаем блокировку, если текущий мьютекс занят, перед переходом к следующему
     }
     return -1;                             // Если все мьютексы заняты, возвращаем -1, чтобы указать, что не удалось создать новый мьютекс
 }
@@ -62,10 +58,9 @@ mutex_create(void) {
 int
 mutex_hold(int mutex_desc) {
     if (mutex_desc < 0 || mutex_desc >= NMUTEX) return -1;
-    if (mt.mutexes[mutex_desc].count < 0 || mt.mutexes[mutex_desc].count >= NMUTEX) return -2;
-    acquire(&mt.mutexes[mutex_desc].spin);
-    int d = holdingsleep(&mt.mutexes[mutex_desc].sleep); // Получение текущего количества удержаний блокировки
-    release(&mt.mutexes[mutex_desc].spin);
+    acquire(&mutexes[mutex_desc].spin);
+    int d = holdingsleep(&mutexes[mutex_desc].sleep); // Получение текущего количества удержаний блокировки
+    release(&mutexes[mutex_desc].spin);
     return d;
 }
 
@@ -76,15 +71,15 @@ mutex_destroy(int mutex_desc) {
 
     if (mutex_unlock(mutex_desc) < 0 && h == 1) return -2;  // Попытка разблокировать мьютекс, если не удалось, возвращаем -2
 
-    acquire(&mt.mutexes[mutex_desc].spin);        // Захват спин-блокировки для безопасного доступа к мьютексу
+    acquire(&mutexes[mutex_desc].spin);        // Захват спин-блокировки для безопасного доступа к мьютексу
 
-    if (mt.mutexes[mutex_desc].count == 0) {      // Если счетчик использования мьютекса равен 0,
-        release(&mt.mutexes[mutex_desc].spin);    // освобождаем спин-блокировку и возвращаем -3, указывая на ошибку
+    if (mutexes[mutex_desc].count == 0) {      // Если счетчик использования мьютекса равен 0,
+        release(&mutexes[mutex_desc].spin);    // освобождаем спин-блокировку и возвращаем -3, указывая на ошибку
         return -3;
     }
 
-    mt.mutexes[mutex_desc].count--;               // Уменьшаем счетчик использования мьютекса
-    release(&mt.mutexes[mutex_desc].spin);        // Освобождаем спин-блокировку после завершения операций с мьютексом
+    mutexes[mutex_desc].count--;               // Уменьшаем счетчик использования мьютекса
+    release(&mutexes[mutex_desc].spin);        // Освобождаем спин-блокировку после завершения операций с мьютексом
 
     return 0;                                      // Возвращаем 0 в случае успешного разрушения мьютекса
 }
@@ -94,12 +89,12 @@ int
 mutex_use(int t) {
     if (t < 0 || t >= NMUTEX) return -1;  // Проверка на корректность индекса мьютекса
 
-    if (mt.mutexes[t].count < 0 || mt.mutexes[t].count >= NMUTEX)
+    if (mutexes[t].count < 0 || mutexes[t].count >= NMUTEX)
         return -2; // Проверяем, что процесс владеет мьютексом с данным дескриптором
 
-    acquire(&mt.mutexes[t].spin);         // Захват блокировки для безопасного доступа к мьютексу
-    mt.mutexes[t].count++;                 // Увеличение счетчика использования мьютекса
-    release(&mt.mutexes[t].spin);          // Освобождение блокировки после операций с мьютексом
+    acquire(&mutexes[t].spin);         // Захват блокировки для безопасного доступа к мьютексу
+    mutexes[t].count++;                 // Увеличение счетчика использования мьютекса
+    release(&mutexes[t].spin);          // Освобождение блокировки после операций с мьютексом
 
     return 0;                              // Возвращаем 0 в случае успешного использования мьютекса
 }
