09397ecd5e6ad987a1609dad7af2702e7610164a
Anna <annalikhtar5@mail.ru>
2024-04-21 00:17:47 +0300

    it's not perfect but it's honest work!

--- user/test.c
+++ user/test.c
@@ -2,8 +2,32 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-
 int
 main(int argc, char *argv[]) {
-    return 0;
+    if (argc != 3 && argc != 4) {
+        printf("Wrong count of arguments!\n");
+        exit(1);
+    }
+
+    int on, tick = 0;
+    if (!strcmp(argv[2], "1")) {
+        on = 1;
+    }
+    else if (!strcmp(argv[2], "0")){
+        on = 0;
+    }
+    else {
+        printf("Wrong input\n");
+        exit(1);
+    }
+    if (argc == 4) {
+        tick = atoi(argv[3]);
+        if (tick < 0) {
+            printf("Incorrect ticks\n");
+            exit(1);
+        }
+    }
+
+    prot(atoi(argv[1]), on, tick);
+    exit(0);
 }
--- user/test_prot.c
+++ user/test_prot.c
@@ -5,5 +5,130 @@
 
 int
 main(int argc, char *argv[]) {
-    return 0;
+    if (argc <= 1) {
+        printf("Wrong count of arguments!\n");
+        exit(1);
+    }
+
+    int fd1[2];
+    int fd2[2];
+    if (pipe(fd1)) {
+        printf("Creation of pipes failed\n");
+        exit(1);
+    }
+
+    if (pipe(fd2)) {
+        close(fd1[0]);
+        close(fd2[0]);
+        printf("Creation of pipes failed\n");
+        exit(1);
+    }
+
+    int cid = fork();
+    if (cid < 0) {
+        close(fd1[0]);
+        close(fd1[1]);
+        close(fd2[0]);
+        close(fd2[1]);
+        printf("Creation of fork failed\n");
+        exit(1);
+    }
+
+    if (cid > 0) {
+        int read_result;
+        char buf;
+
+        if (close(fd1[0]) < 0) {
+            close(fd1[1]);
+            close(fd2[0]);
+            close(fd2[1]);
+            printf("Closing of fork failed fd1[0] (parent)\n");
+            exit(1);
+        }
+        if (close(fd2[1]) < 0) {
+            close(fd1[1]);
+            close(fd2[0]);
+            printf("Closing of fork failed fd2[0] (parent)\n");
+            exit(1);
+        }
+
+        if (write(fd1[1], argv[1], strlen(argv[1])) != strlen(argv[1])) {
+            close(fd1[1]);
+            close(fd2[0]);
+            printf("Writing to pipe failed!\n");
+            exit(1);
+        }
+        if (close(fd1[1]) < 0) {
+            close(fd2[0]);
+            printf("Closing of fork failed fd1[1] (parent)\n");
+            exit(1);
+        }
+
+        while (1) {
+            read_result = read(fd2[0], &buf, 1);
+            if (read_result < 0) {
+                close(fd2[0]);
+                printf("Reading from pipe failed!\n");
+                exit(1);
+            }
+            if (read_result == 0) break;
+
+            printf("%d: received '%c'\n", getpid(), buf);
+        }
+        if (close(fd2[0]) < 0) {
+            printf("Closing of fork failed fd2[1] (parent)\n");
+            exit(1);
+        }
+
+    }
+    else {
+        int read_result;
+        char buf;
+
+        if (close(fd1[1]) < 0) {
+            close(fd1[0]);
+            close(fd2[0]);
+            close(fd2[1]);
+            printf("Closing of fork failed fd1[0] (parent)\n");
+            exit(1);
+        }
+        if (close(fd2[0]) < 0) {
+            close(fd1[0]);
+            close(fd2[1]);
+            printf("Closing of fork failed fd2[0] (parent)\n");
+            exit(1);
+        }
+
+        while (1) {
+            read_result = read(fd1[0], &buf, 1);
+            if (read_result < 0) {
+                close(fd1[0]);
+                close(fd2[1]);
+                printf("Reading from pipe failed!\n");
+                exit(1);
+            }
+
+            if (read_result == 0) break;
+
+            printf("%d: received '%c'\n", getpid(), buf);
+
+            if (write(fd2[1], &buf, 1) != 1) {
+                close(fd1[0]);
+                close(fd2[1]);
+                printf("Writing to pipe failed!\n");
+                exit(1);
+            }
+        }
+
+        if (close(fd1[0]) < 0) {
+            close(fd2[1]);
+            printf("Closing of fork failed fd1[0] (parent)\n");
+            exit(1);
+        }
+        if (close(fd2[1]) < 0){
+            printf("Closing of fork failed fd2[1] (parent)\n");
+            exit(1);
+        }
+    }
+    exit(0);
 }
